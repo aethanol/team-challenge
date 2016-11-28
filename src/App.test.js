2ae384a1d25d66e646f5a48c75a15515e4eb1360
Oorja Chowdhary <oorjac@uw.edu>
2016-11-27 17:10:22 -0800

    added describes to clean up tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {RequiredInput} from './TeamSignUp';
-import {shallow} from 'enzyme';
+import { RequiredInput } from './TeamSignUp';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+describe('<RequiredInput /> name component', () => {
+  it("if user hasnt entered a name", () => {
+    const wrapper = shallow(<RequiredInput value={''} errorMessage={"we need to know your name"} />);
+    expect(wrapper.find('.error-missing').text()).toEqual("we need to know your name");
 
-it("if user hasnt entered a name", () => {
-      const wrapper = shallow(<RequiredInput value={''} errorMessage={"we need to know your name"}  />);
-      expect(wrapper.find('.error-missing').text()).toEqual("we need to know your name");
+  });
+
+  it("if user has entered a name", () => {
+    const wrapper = shallow(<RequiredInput value={'Hey'} />);
+    expect(wrapper.find('.error-missing').length).toEqual(0);
+
+  });
+});
+
+describe('<RequiredInput /> password component', () => {
+   it("if user hasnt entered a password", () => {
+    const wrapper = shallow(<RequiredInput value={''} errorMessage={"your password can't be blank"} />);
+    expect(wrapper.find('.error-missing').text()).toEqual("your password can't be blank");
+
+  });
+  
+  it("if user has entered a password", () => {
+    const wrapper = shallow(<RequiredInput value={'password'} />);
+    expect(wrapper.find('.error-missing').length).toEqual(0);
+
+  });
+
 
 });
 
-it("if user hasnt entered a password", () => {
-      const wrapper = shallow(<RequiredInput value={''} errorMessage={"your password can't be blank"}  />);
-      expect(wrapper.find('.error-missing').text()).toEqual("your password can't be blank");
-
-});
-
-it("if user has entered a password or name", () => {
-      const wrapper = shallow(<RequiredInput value={'Hey'} />);
-      expect(wrapper.find('.error-missing').length).toEqual(0);
-
-});
