86326fe2f8098aac075c38f2ac8f8ae06e760d1c
Ethan Anderson <aethan@uw.edu>
2016-11-27 17:45:42 -0800

    Fix form errors

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { EmailInput, RequiredInput, BirthdayInput, PasswordConfirmationInput } from './TeamSignUp';
+import {shallow} from 'enzyme';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
